2099ac73171663b28ff1008a70ff827f976fc9fd
Austin Fox <44780320+Foxbone@users.noreply.github.com>
2020-03-15 10:16:19 -0500

    Create Lisp_Ops.java

---
+++ Lisp_Ops.java
@@ -0,0 +1,24 @@
+public enum Operation {
+    // Arithmetic
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    MOD,
+    // Rounding
+    ROUND,
+    FLOOR,
+    CEIL,
+    // Trigonometric
+    SIN,
+    COS,
+    TAN,
+    HYPOT,
+    // Other
+    LOG,
+    RAND,
+    SET,
+    PRINT,
+    READ,
+    NULL
+}
